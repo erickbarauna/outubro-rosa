ee876aa77900bda39d5136a013bc30a951fa4c84
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-10-14 11:57:03 -0300

    responsividade menu
    ajuste posicionamento menu

--- js/script.js
+++ js/script.js
@@ -21,6 +21,9 @@ let show = true
 const menuToggle = menuSection.querySelector(".menu-toggle");
 
 function Clique() {
+
+    document.body.style.overflow = show ? "hidden" : "initial";
+
     document.querySelector(".menu-section").classList.toggle("on", show) // Essa função toggle põe ou tira uma class da tag, no caso (".menu-section")
     show = !show;
 }
