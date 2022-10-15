77c3c1facb134efd2d085d0466d4c85eea92d944
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-10-15 06:51:52 -0300

    páginas do menu criadas

--- js/script.js
+++ js/script.js
@@ -3,18 +3,16 @@ document.getElementById("radio1").checked = true;
 
 setInterval( function(){
     nextImage();
-}, 11500)
-
+}, 15000)
 
 function nextImage(){
     count++;
     if(count>6){
         count = 1;
     }
-
     document.getElementById("radio"+count).checked = true;
-
 }
+
 
 let show = true
 
